9afe9fab3676e89a3d0e0eab5f5d100bd753e27f
jbhafner <brian_hafner@yahoo.com>
2017-11-04 14:15:34 -0700

    fix 2 for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 // ------------ Sets up the Express app --------------- //
 // ------------ to handle data parsing --------------- //
